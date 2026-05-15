e862d780b1324c65cd7a1951700e5bfaaafb1d68
Matthew Chicano <matthew.poole485@gmail.com>
2026-05-15 17:37:15 -0400

    configured switch statement for search inquries

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,15 +1,36 @@
 const btnSearch = document.getElementById("search");
 const btnReset = document.getElementById("reset");
 
-
+// Search =====================================================================
 function search() {
-    const input = document.getElementById("input").value;
-    
+    const input = document.getElementById("input").value.toLowerCase().trim();
+
+    fetch("travel_recommendation_api.json")
+    .then(response => response.json())
+    .then(data => {
+        let dest = input.toLowerCase();
+
+        switch (true) {
+            case dest.includes("beach"):
+                console.log("beaches");
+                break;
+            
+            case dest.includes("temple"):
+                console.log("temples");
+                break;
+
+            case dest.includes("countr"):
+                console.log("countries");
+                break;
+            
+            default:
+                console.log("nope");
+        }
+    })
 }
 
-function reset() {
-
-}
+// Reset ======================================================================
+function reset() {document.getElementById("input").value = "";}
 
 btnSearch.addEventListener("click", search);
 btnReset.addEventListener("click", reset);
