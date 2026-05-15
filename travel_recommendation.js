39400cd08beaf1644c863836f6b82f1e0d158eb6
Matthew Chicano <matthew.poole485@gmail.com>
2026-05-15 11:39:32 -0400

    building .js file

---
+++ travel_recommendation.js
@@ -0,0 +1,15 @@
+const btnSearch = document.getElementById("search");
+const btnReset = document.getElementById("reset");
+
+
+function search() {
+    const input = document.getElementById("input").value;
+    
+}
+
+function reset() {
+
+}
+
+btnSearch.addEventListener("click", search);
+btnReset.addEventListener("click", reset);
