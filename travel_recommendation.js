08186e2f92d1b7b806280df1317976ab370f4850
Matthew Poole Chicano <matthew.poole485@gmail.com>
2026-05-16 10:30:47 -0400

    fixed rec grid sizing

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,11 +2,11 @@ const btnSearch = document.getElementById("search");
 const btnReset = document.getElementById("reset");
 const msgW = document.getElementById("msgWelcome");
 const grid = document.getElementById("grid");
-grid.innerHTML = "";
 
 // ================================= Search ===================================
 function search() {
     const dest = document.getElementById("input").value.toLowerCase().trim();
+    grid.innerHTML = "";
     
     fetch("travel_recommendation_api.json")
     .then(response => response.json())
@@ -19,7 +19,7 @@ function search() {
                 for (const beach of beaches) {
                     const card = document.createElement("div");
                     card.classList.add("card");
-                    card.innerHTML += `<img src="${beach.imageUrl}" height="405" width="720" style="justify-self: center">`;
+                    card.innerHTML += `<img src="${beach.imageUrl}">`;
                     card.innerHTML += `<h3>${beach.name}</h3>`;
                     card.innerHTML += `<p>${beach.description}</p>`;
                     grid.appendChild(card);
@@ -33,7 +33,7 @@ function search() {
                 for (const temple of temples) {
                     const card = document.createElement("div");
                     card.classList.add("card");
-                    card.innerHTML += `<img src="${temple.imageUrl}" height="405" width="720" style="justify-self: center">`;
+                    card.innerHTML += `<img src="${temple.imageUrl}">`;
                     card.innerHTML += `<h3>${temple.name}</h3>`;
                     card.innerHTML += `<p>${temple.description}</p>`;
                     grid.appendChild(card);
@@ -49,7 +49,7 @@ function search() {
                     for (const city of cities) {
                         const card = document.createElement("div");
                         card.classList.add("card");
-                        card.innerHTML += `<img src="${city.imageUrl}" height="405" width="720" style="justify-self: center">`;
+                        card.innerHTML += `<img src="${city.imageUrl}">`;
                         card.innerHTML += `<h3>${city.name}</h3>`;
                         card.innerHTML += `<p>${city.description}</p>`;
                         grid.appendChild(card);
@@ -67,6 +67,7 @@ function search() {
 // ================================== Reset ===================================
 function reset() {
     document.getElementById("input").value = "";
+    grid.innerHTML = "";
     msgW.hidden = false;
     grid.hidden = true;
 }
