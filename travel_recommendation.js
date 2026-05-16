23bfb658f80a28f1f4c52e1d904c17910300baa9
Matthew Chicano <matthew.poole485@gmail.com>
2026-05-16 00:38:28 -0400

    configured .js file to return recomendations depending on user input

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,40 +1,65 @@
 const btnSearch = document.getElementById("search");
 const btnReset = document.getElementById("reset");
 const msgW = document.getElementById("msgWelcome");
+const grid = document.getElementById("grid");
+grid.innerHTML = "";
 
 // ================================= Search ===================================
 function search() {
+    const dest = document.getElementById("input").value.toLowerCase().trim();
+    
     fetch("travel_recommendation_api.json")
     .then(response => response.json())
     .then(data => {
-        const dest = document.getElementById("input").value.toLowerCase().trim();
-        const grid = document.getElementById("grid");
-        const card = document.createElement("div");
-
         switch (true) {
             case dest.includes("beach"):
-                msgW.hidden = true;
-
                 const beaches = data.beaches
+                msgW.hidden = true;
+                grid.hidden = false;
                 for (const beach of beaches) {
+                    const card = document.createElement("div");
                     card.classList.add("card");
-                    card.innerHTML += `<img src="${beaches.imageUrl}">`;
-                    card.innerHTML += `<h3>${beaches.name}</h3>`;
-                    card.innerHTML += `<p>${beaches.description}</p>`;
+                    card.innerHTML += `<img src="${beach.imageUrl}" height="405" width="720" style="justify-self: center">`;
+                    card.innerHTML += `<h3>${beach.name}</h3>`;
+                    card.innerHTML += `<p>${beach.description}</p>`;
                     grid.appendChild(card);
                 };
                 break;
             
             case dest.includes("temple"):
+                const temples = data.temples
                 msgW.hidden = true;
+                grid.hidden = false;
+                for (const temple of temples) {
+                    const card = document.createElement("div");
+                    card.classList.add("card");
+                    card.innerHTML += `<img src="${temple.imageUrl}" height="405" width="720" style="justify-self: center">`;
+                    card.innerHTML += `<h3>${temple.name}</h3>`;
+                    card.innerHTML += `<p>${temple.description}</p>`;
+                    grid.appendChild(card);
+                };
                 break;
 
             case dest.includes("countr"):
+                const countries = data.countries
                 msgW.hidden = true;
+                grid.hidden = false;
+                for (const country of countries) {
+                    const cities = country.cities;
+                    for (const city of cities) {
+                        const card = document.createElement("div");
+                        card.classList.add("card");
+                        card.innerHTML += `<img src="${city.imageUrl}" height="405" width="720" style="justify-self: center">`;
+                        card.innerHTML += `<h3>${city.name}</h3>`;
+                        card.innerHTML += `<p>${city.description}</p>`;
+                        grid.appendChild(card);
+                    }
+                };
                 break;
             
             default:
                 msgW.hidden = true;
+                grid.hidden = true;
         }
     })
 }
@@ -43,6 +68,7 @@ function search() {
 function reset() {
     document.getElementById("input").value = "";
     msgW.hidden = false;
+    grid.hidden = true;
 }
 
 btnSearch.addEventListener("click", search);
